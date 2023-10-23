0ebd506adb00059a2f839be09f637e629f314446
Omar Afosh <omarafosh@gmail.com>
2023-10-23 13:14:33 +0300

    Card Orphans

---
+++ app/Components/Cards/CardOrphanComponent.php
@@ -0,0 +1,36 @@
+<div class="card">
+    <div class="orphans-card-header">
+        كفالة الايتام
+        <p>ترخيص: 68768786786</p>
+    </div>
+    <div class="orphans-personal">
+        <div class="data">
+            <p>الاسم : <span>7686867</span></p>
+            <p>العمر : <span>76867867</span></p>
+            <p>الجنس : <span>6786786786</span></p>
+        </div>
+        <img src="assets/imgs/female.png" alt="">
+
+    </div>
+
+
+
+    <div class="orphans-nationl">
+        <img src="assets/flags/16x16/us.png" width="20" height="20" style="margin: 8px 5px;" title="بولندا">
+        <span>
+            بولندا
+        </span>
+    </div>
+    <div class="orphans-row2">
+        <div class="orphans-options">
+            <label for="month">تخصم تلقائيا (شهرياً) <span>150 QAR</span>
+                <input type="radio" name="type" id="month" data-mony="150"></label>
+            <label for="year"> يتم اخد موافقتك للتجديد (سنوياً) <span>1800 QAR</span>
+                <input type="radio" name="type" id="year" data-mony="1800"></label>
+        </div>
+    </div>
+    <div class="orphans-amount">
+        <input class="orphans-mony input-text" type="text" disabled>
+        <a href="../../donate/pages/donate-fast.php" class="form-button">اكفلني</a>
+    </div>
+</div>
